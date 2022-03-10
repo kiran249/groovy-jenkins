d46e2e61dae48dd0e12f87a0568a9bd6ad440076
kiran249 <60708138+kiran249@users.noreply.github.com>
2022-03-10 16:53:59 +0530

    Update helloworld.groovy

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,3 +1,13 @@
 def hello(iloveyou){
-  echo "hi ${iloveyou}"
+  echo "hello ${iloveyou}"
 } 
+
+def hello1(iloveyou){
+  echo "hello1 ${iloveyou}"
+} 
+
+
+def hello2(iloveyou){
+  echo "hello2 ${iloveyou}"
+} 
+
