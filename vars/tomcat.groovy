effd253667019462bb44e27d7c82eea8b8702126
kiran249 <60708138+kiran249@users.noreply.github.com>
2022-03-11 12:56:19 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -1,7 +1,7 @@
-def tomcat(){
-  sshagent(['kiran']) {
-                    sh 'scp -o StrictHostKeyChecking=no target/*.war ec2-user@44.201.109.181:/home/ec2-user/apache-tomcat-8.5.76/webapps/'
-                    sh 'ssh ec2-user@44.201.109.181 /home/ec2-user/apache-tomcat-8.5.76/bin/shutdown.sh'
-                    sh 'ssh ec2-user@44.201.109.181 /home/ec2-user/apache-tomcat-8.5.76/bin/startup.sh'
+def call("ip,user,credential){
+  sshagent(['credential']) {
+    sh 'scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/home/ec2-user/apache-tomcat-8.5.76/webapps/'
+    sh 'ssh ${user}@${ip} /home/ec2-user/apache-tomcat-8.5.76/bin/shutdown.sh'
+    sh 'ssh ${user}@${ip} /home/ec2-user/apache-tomcat-8.5.76/bin/startup.sh'
                 }
             }
