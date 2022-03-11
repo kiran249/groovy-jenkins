64cd47b0c885c2d374627da416587cffdb9da7c6
kiran249 <60708138+kiran249@users.noreply.github.com>
2022-03-11 11:37:29 +0530

    Create tomcat.groovy

---
+++ vars/tomcat.groovy
@@ -0,0 +1,5 @@
+sshagent(['kiran']) {
+                    sh 'scp -o StrictHostKeyChecking=no target/*.war ec2-user@44.201.109.181:/home/ec2-user/apache-tomcat-8.5.76/webapps/'
+                    sh 'ssh ec2-user@44.201.109.181 /home/ec2-user/apache-tomcat-8.5.76/bin/shutdown.sh'
+                    sh 'ssh ec2-user@44.201.109.181 /home/ec2-user/apache-tomcat-8.5.76/bin/startup.sh'
+                }
